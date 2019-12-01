fa9a9166d1be70e479c8e74ad3f8075f25186724
Dahir Muhammad Dahir <dahirmuhammaddahir@gmail.com>
2019-12-01 21:35:50 +0100

    completed the string methods lesson

---
+++ lessons/stringMethods.py
@@ -0,0 +1,41 @@
+
+state = "kaduna"
+planet = "earth"
+
+sometext = "long long a this four nations live together in harmony, then everything changes when the fire nation attack"
+
+# print(state.capitalize())
+
+# print(state.count('k', ))
+
+filename = "somefile.html"
+
+text = "nothingistrue"
+
+someUpperText = "JUST SOME UPPER STRING"
+
+someNumber = "10253041"
+
+dump_text = "old good bad good small good new good"
+
+# print(sometext.count(""))
+
+# print(text.endswith("time"))
+
+# print(someNumber.isnumeric())
+
+# print(someUpperText.isupper())
+# print(someUpperText.lower())
+
+# print(dump_text.replace('good', 'bad'))
+
+list_of_items = "rice, chicken, fish, goat"
+
+nasty_text = "**sometext**"
+
+nastier_text = "          sometext          "
+print(nastier_text)
+
+# print(list_of_items.split(","))
+
+print(nastier_text.strip(" "))

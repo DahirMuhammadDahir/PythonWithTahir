1d49a3978009b794be0c5d7fbc031c3fc0499165
Dahir Muhammad Dahir <dahirmuhammaddahir@gmail.com>
2019-12-02 00:29:36 +0100

    made string interpolation video

---
+++ lessons/stringInterplation.py
@@ -0,0 +1,9 @@
+name = "Dahir Muhammad"
+Age = 30
+time = 25
+
+state = "Zamfara"
+
+print("my name is " + name + " and I am " + str(Age) + " years")
+
+print(f"my name is {name} and I am {Age + time} years")
